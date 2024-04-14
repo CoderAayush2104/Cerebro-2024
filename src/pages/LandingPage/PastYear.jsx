15a6bc0de28aaa5da422c358ae5605d44edff910
aayush.2104 <aayush.2104@gmail.com>
2024-04-15 03:53:17 +0530

    Added sample carousel and made footer responsive

--- src/pages/LandingPage/PastYear.jsx
+++ src/pages/LandingPage/PastYear.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./styles/pastYear.css"
+import Carousel from '../../components/Carousel'
 const PastYear = () => {
  
   return (
     <div className="past-year-section">
         <span className="past-year-title">Past Year Glimpses</span>
-        
+       <Carousel/>
         
     </div>
   )
