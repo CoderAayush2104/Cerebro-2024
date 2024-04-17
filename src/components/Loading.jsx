6836cef42622387fe98dc26756261a788c7f2dca
aayush.2104 <aayush.2104@gmail.com>
2024-04-17 15:45:27 +0530

    Added loading state again

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import "./loading.css"
+import React from "react";
+import "./loading.css";
 const Loading = () => {
   return (
-    <div className='loading-container'><img className="loading-spinner" src='/public/logo.svg'></img></div>
-  )
-}
+    <div className="loading-container">
+      <img className="loading-spinner" src="/logo.svg"></img>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
