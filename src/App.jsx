4c97e60fcdb097ae7261bb84068e021b1b11e340
aayush.2104 <aayush.2104@gmail.com>
2024-04-17 15:42:28 +0530

    Added loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage/LandingPage';
@@ -6,16 +6,24 @@ import "./App.css"
 import EventPage from "./pages/Events/EventPage"
 import TeamPage from './pages/Teams/TeamPage';
 import TimelinePage from './pages/Timeline/TimelinePage';
+import Loading from './components/Loading';
 function App() {
+  const [loading,setLoading] = useState(true);
+  setTimeout(() => setLoading(false),3000);
   return (
     <BrowserRouter> 
- 
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route path = "/events" element = {<EventPage/>}/>
-        <Route path = "/teams" element = {<TeamPage/>}/>
-        <Route path = "/timeline" element = {<TimelinePage/>}/>
-      </Routes>
+    {loading ? (
+      <Loading/>
+    ) : (
+
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route path="/events" element={<EventPage />} />
+          <Route path="/teams" element={<TeamPage />} />
+          <Route path="/timeline" element={<TimelinePage />} />
+        </Routes>
+    )}
+    
     </BrowserRouter>
   );
 }
---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./loading.css"
+const Loading = () => {
+  return (
+    <div className='loading-container'><img className="loading-spinner" src='/public/logo.svg'></img></div>
+  )
+}
+
+export default Loading
