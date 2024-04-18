e38fd4911483e41b11c76dd49789c664ef4b78dc
aayush.2104 <aayush.2104@gmail.com>
2024-04-18 15:38:19 +0530

    Added event card images

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -7,7 +7,7 @@ const EventCard = ({ event }) => {
     <div className="event-page-card">
       <div className="event-page-card-content">
         <div className="event-page-card-front">
-          <img src={`/assets/event-card/event-info.png`} alt="Event Info" loading="lazy" />
+          <img src={`/assets/event-card/events/${event.image_name}`} className="event-image" alt="Event Info" loading="lazy" />
           <span className="event-page-card-title">{event.title}</span>
           <span className="event-page-card-info">{event.timings}</span>
         </div>
